55b9a8a95f5878313cd0a5f8e0ff37b7a67cbea2
Hridaya Agrawal <hridayahoney@gmail.com>
2022-03-28 08:38:14 +0530

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -46,10 +46,10 @@ const Welcome = () => {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white py-1">
-            Send Crypto <br /> across the world
+            Transfer ETH <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Transfer Crypto with TransCrypt
+            Explore the crypto world. Transfer ETH with TransCrypt
           </p>
           {!currentAccount && (
             <button
